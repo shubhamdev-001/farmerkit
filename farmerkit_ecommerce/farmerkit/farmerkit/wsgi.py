7d23a171ed35d3d1f390f7482bd51665a28a6bee
shubhamdev-001 <shubhamjangid52175@gmail.com>
2025-08-02 12:52:12 +0530

    path

--- farmerkit_ecommerce/farmerkit/farmerkit/wsgi.py
+++ farmerkit_ecommerce/farmerkit/farmerkit/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'farmerkit.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'farmerkit.farmerkit.settings')
 
 application = get_wsgi_application()
+
